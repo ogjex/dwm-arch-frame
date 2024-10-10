a63eacd38ed9ea490fed67b4410db522273812c1
ogjex <magnuha@gmail.com>
2024-10-10 09:11:47 +0200

    updated font sizes for arch hi dpi

--- config.h
+++ config.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetbrainsMono:size=10" };
-static const char dmenufont[]       = "JetbrainsMono:size=10";
+static const char *fonts[]          = { "JetbrainsMono:size=16" };
+static const char dmenufont[]       = "JetbrainsMono:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
